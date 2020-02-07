9ba7fc1fec05f221c9f50626f68db48ca75b6e6e
hiyv7272 <hiyv7272@gmail.com>
2020-02-08 05:28:59 +0900

    [ seller > 회원가입 API구성 ] - seller/sign-up 엔드포인트 수정 - accounts, seller 테이블 INSERT INTO, 트랜잭션 설정 - 아이디, 비밀번호 유효성검사 추가 - CORS 추가, flask-cors 추가

--- seller/service/seller_service.py
+++ seller/service/seller_service.py
@@ -1,16 +1,43 @@
 import bcrypt
+import re
 
-class SellerService:
+from flask      import request, jsonify, abort
+from flask.json import JSONEncoder
 
+
+class SellerService():
+    
     def __init__(self, seller_dao):
         self.seller_dao = seller_dao
+    
+    def create_new_seller(self, new_seller):
 
-    def create_new_seller(self, new_seller):      
-        new_seller['password'] = bcrypt.hashpw(  
-            new_seller['password'].encode('UTF-8'),
-            bcrypt.gensalt()
-        )
+        try:
 
-        new_seller_id = self.seller_dao.insert_seller(new_seller)
+            check_account = re.compile("^[A-Za-z0-9]+[A-Za-z0-9-_]{5,20}$")
+            check_password = re.compile("^[A-Za-z0-9~`!@#$%\^&*()-+=]{4,}$")
+            check_number = re.compile("^[0-9]{10,11}$")
+
+            if not check_account.match(new_seller['account']):
+                abort (400, description="INVALID_ACCOUNT")
+            
+            if not check_password.match(new_seller['password']):
+                abort (400, description="INVALID_PASSWORD")
+
+            if not check_number.match(new_seller['cs_phone_number']):
+                abort (400, description="INVALID_PHONE_NUMBER")
+            
+            if not check_number.match(new_seller['mobile_number']):
+                abort (400, description="INVALID_MOBILE_NUMBER")
+
+            new_seller['password'] = bcrypt.hashpw(  
+                new_seller['password'].encode('UTF-8'),
+                bcrypt.gensalt()
+            )
+
+            insert_new_seller = self.seller_dao.insert_seller(new_seller)
+            
+            return insert_new_seller
         
-        return new_seller_id
+        except KeyError:
+            abort (400, description="INVALID_KEY")
--- seller/view/seller_view.py
+++ seller/view/seller_view.py
@@ -1,6 +1,7 @@
 from flask      import request, jsonify
 from flask.json import JSONEncoder
 
+
 class CustomJSONEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, set):
@@ -12,10 +13,27 @@ class SellerView:
 
     def create_endpoints(app, services):
 
+        app.json_encoder = CustomJSONEncoder
         seller_service  = services.seller_service
 
+        @app.errorhandler(400)
+        def bad_request(error):
+            response = jsonify({'error_message': error.description})
+            response.status_code = 400
+            return response
+
+
+        @app.errorhandler(404)
+        def page_not_found(error):
+            return "INVALID_URL", 404
+        
+        @app.errorhandler(405)
+        def method_not_allowed(error):
+            return "INVALID_REQUEST", 405
+        
         @app.route("/seller/sign-up", methods=['POST'])
         def sign_up():
+
             new_seller = request.json
             new_seller = seller_service.create_new_seller(new_seller)
 
