5ac631eaaca7a4ba7b4fcda16e9982f8d0166c79
hiyv7272 <hiyv7272@gmail.com>
2020-02-16 01:56:54 +0900

    [ seller/image-upload 엔드포인트 ] - seller/image-upload 엔드포인트 구현 - jpg확장자만 받도록 구현 - S3 이미지 업로드 및 url 리턴

--- seller/service/seller_service.py
+++ seller/service/seller_service.py
@@ -1,14 +1,21 @@
 import bcrypt
 import jwt
 import re
+import os
+import io
+import boto3
 
-from flask      import request, jsonify, current_app, g, abort
-from flask.json import JSONEncoder
-
-from datetime   import datetime, timedelta
+from PIL                import Image
+from uuid               import uuid4
+from werkzeug.utils     import secure_filename
+from flask              import request, jsonify, current_app, g, abort
+from flask.json         import JSONEncoder
+from datetime           import datetime, timedelta
+from smart_open import open
 
 
 class SellerService():
+    ALLOWED_EXTENSIONS = set(['jpg'])
     
     def __init__(self, seller_dao):
         self.seller_dao = seller_dao
@@ -118,3 +125,48 @@ class SellerService():
 
         if user_authorities_id > 2:
             abort (400, description="INVALID_KEY")
+
+    """
+    리사이즈하는 파일의 확장자를 체크하는 함수
+    """
+    def allowed_image_file(self, filename):
+        return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in self.ALLOWED_EXTENSIONS
+
+    """
+    이미지를 S3에 업로드 하는 함수
+    """
+    def upload_image(self, upload_image, upload_filename):
+        access_key = current_app.config['S3_ACCESS_KEY']
+        secret_key = current_app.config['S3_SECRET_KEY']
+        bucket = current_app.config['S3_BUCKET']
+        bucket_url = current_app.config['S3_BUCKET_URL']
+        obj_url = f's3://{access_key}:{secret_key}@{bucket}/{upload_filename}'
+
+        with open(obj_url ,'wb') as fout:
+            fout.write(upload_image.getvalue())
+            uploaded_url = f'{bucket_url}{upload_filename}'
+
+        return uploaded_url
+
+    """
+    원본 이미지를 S3에 업로드 하는 함수
+    """
+    def upload_original_image(self, image_file):
+
+        filename, file_extension = os.path.splitext(image_file.filename)
+        
+        # 원본 이미지를 S3에 업로드하고 URL을 리턴
+        img_urls = dict()
+        with Image.open(image_file) as opened_image:
+            img_io = io.BytesIO()
+            opened_image.save(img_io, 'jpeg')
+            img_io.seek(0)
+
+            uploaded_name = str(uuid4()) + filename + file_extension
+            uploaded_url=self.upload_image(img_io, uploaded_name)
+            img_urls['uploaded'] = 1
+            img_urls['fileName'] = uploaded_name
+            img_urls['url'] = uploaded_url
+
+        return img_urls
